b05d2028e44bc693d237687209581c0d9b11f424
Sudhu1705s <sudhanshuraj1705@gmail.com>
2026-01-08 15:21:12 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -4,9 +4,10 @@ Location: telegram_scheduler_bot/core/__init__.py
 Purpose: Core logic package initialization
 """
 
-from .rate_limiter import AggressiveRateLimiter
+from .rate_limiter import BalancedRateLimiter
 from .retry_system import SmartRetrySystem
 from .sender import ParallelSender
 from .scheduler_core import SchedulerCore
 
 __all__ = ['AggressiveRateLimiter', 'SmartRetrySystem', 'ParallelSender', 'SchedulerCore']
+
